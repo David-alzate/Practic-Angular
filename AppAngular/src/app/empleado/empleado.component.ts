2b86f21ccb77e45d04945342e488188153466de5
David-alzate <alzatedavid0126@gmail.com>
2024-06-17 16:07:55 -0500

    Property Binding y Event Binding

--- AppAngular/src/app/empleado/empleado.component.ts
+++ AppAngular/src/app/empleado/empleado.component.ts
@@ -11,5 +11,24 @@ export class EmpleadoComponent {
   apellido: String = "Alzate"
   edad:number = 19
   // empresa:String = "UCO"
+  textLogin:String = "No hay nadie registrado"
+
+  habilitarButton=false;
+  usuarioLogeado=false;
+
+  getRegistroUsuario(){
+    this.usuarioLogeado = false; 
+  }
+
+  setUsuarioRegistrado(event:Event){
+    //alert("El usuario se ha registrado")
+    // this.textLogin = "El usuario se ha registrado"
+
+    if ((<HTMLInputElement>event.target).value == "si"){
+      this.textLogin = "El usuario se ha registrado";
+    }else{
+      this.textLogin = "No hay nadie registrado";
+    }
+  }
 
 }
