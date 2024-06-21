a8203b533f9f8573e86ef51be167c1ddd679638d
David-alzate <alzatedavid0126@gmail.com>
2024-06-20 20:03:32 -0500

    Creacion de formulario de creacion para empleado

--- EmpleadosApp/src/app/app.component.ts
+++ EmpleadosApp/src/app/app.component.ts
@@ -10,11 +10,21 @@ export class AppComponent {
   titulo = 'Listado de Empleados';
 
   empleados:Empleado[] = [
-
     new Empleado("David", "Alzate", "Estudiante", 4500),
     new Empleado("Santiago", "Jimenez", "Ing.Sistemas", 7000),
     new Empleado("Juan", "Lopez", "Estudiante", 4000),
     new Empleado("Esteban", "Rios", "Administrador", 4500),
   ]
+
+  nombre:String = "";
+  apellido:String = "";
+  cargo:String = "";
+  salario:number = 0;
+
+  crearEmpleado(){
+    let EmpleadoNuevo = new Empleado(this.nombre, this.apellido, this.cargo, this.salario);
+    this.empleados.push(EmpleadoNuevo);
+  }
+
 }
  
