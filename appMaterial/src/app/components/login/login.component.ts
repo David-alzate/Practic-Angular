62aad0ae88c82453f0017d87c5a075dbb257ad3a
David-alzate <alzatedavid0126@gmail.com>
2024-06-29 16:57:08 -0500

    Creacion de pantalla de login utilizando angular material

---
+++ appMaterial/src/app/components/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit{
+  form: FormGroup;
+  loading = false;
+
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar){
+    this.form = this.fb.group({
+      usuario: ['', Validators.required], 
+      password: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  
+ingresar(){
+  const usuario = this.form.value.usuario;
+  const password = this.form.value.password;
+
+  // Datos login prueba
+  if(usuario == 'david' && password == '123'){
+    // Redireccionamos a la app 
+    this.fakeLoadin();
+  } else {
+    // Mensaje de error
+    this.error();
+    this.form.reset();
+
+  }
+} 
+
+error(){
+  this._snackBar.open('Usuario o contraseña incorrectas', '', {
+    duration: 5000,
+    horizontalPosition: 'center',
+    verticalPosition: 'top',
+  })
+}
+
+fakeLoadin(){
+  this.loading = true;
+  setTimeout(() => {
+    this.loading = false;
+  }, 1500);
+}
+}
