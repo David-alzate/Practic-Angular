c492da652b8e03f9f96b2af932973d815140c740
David-alzate <alzatedavid0126@gmail.com>
2024-06-30 16:51:09 -0500

    Table, Filter, Paginator y Short utilizado angular material

--- appMaterial/src/app/components/dashboard/usuarios/usuarios.component.ts
+++ appMaterial/src/app/components/dashboard/usuarios/usuarios.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Usuario } from 'src/app/interfaces/usuario';
 
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
-export class UsuariosComponent {
+export class UsuariosComponent{
 
+  listUsuarios: Usuario[] = [
+    {usuario: "Monno", nombre: 'David', apellido: "Alzate", sexo: 'Masculino'},
+    {usuario: "Monno1", nombre: 'Santiago', apellido: "Jimenez", sexo: 'Masculino'},
+    {usuario: "MarlonPerl", nombre: 'Marlon', apellido: "Perla", sexo: 'Masculino'},
+    {usuario: "Lali", nombre: 'Laura', apellido: "Jimenez", sexo: 'Femenino'},
+    {usuario: "Rack", nombre: 'Jimmy', apellido: "Tabares", sexo: 'Masculino'},
+    {usuario: "War", nombre: 'Edwar', apellido: "Tabares", sexo: 'Masculino'},
+    {usuario: "estefa", nombre: 'estefania', apellido: "sanchez", sexo: 'Femenino'},
+    {usuario: "bam", nombre: 'Jose', apellido: "Ramirez", sexo: 'Masculino'},
+  
+  ];
+
+  displayedColumns: string[] = ['usuario', 'nombre', 'apellido', 'sexo', 'acciones'];
+  dataSource = new MatTableDataSource(this.listUsuarios);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
+
+
